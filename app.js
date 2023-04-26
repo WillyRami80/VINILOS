e45b814da7087e98a3afc95f6370b212577834f3
WillyRami80 <waramirez80@gmail.com>
2023-04-25 23:54:03 -0500

    Se inicia trabajo con rutas. Aún hay algunos errores

--- app.js
+++ app.js
@@ -10,3 +10,31 @@ app.listen(3000, () => {
 app.get('/' , (req, res) => {
     res.sendFile(__dirname + '/views/home.html')
 });
+
+app.get('inicio', function(req, res) {
+    res.send('Inicio');
+});    
+
+app.get('/quienes', function(req, res) {
+    res.send('Quiénes Somos');
+});
+
+app.get('/Registrate', function(req, res) {
+    res.send('Regístrate');
+});    
+
+app.get('/iniciar', function(req, res) {
+    res.send('Iniciar');
+});
+
+app.get('/carrito', function(req, res) {
+    res.send('Carrito');
+});
+
+app.get('/buscar disco', function(req, res) {
+    res.send('Busca tu Disco');
+});
+
+app.get('/buscar', function(req, res) {
+    res.send('Buscar');
+});
